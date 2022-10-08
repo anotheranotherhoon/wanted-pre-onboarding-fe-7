f700c21368fff3c2460f2bf63c8549fe88d22b4d
LeeChangHoon <changhoon1030@gmail.com>
2022-10-08 22:46:31 +0900

    Feat : 유효성 검사 추가

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,18 +1,23 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom';
 import BorderLayout from '../components/BorderLayout'
-import axiosInstance  from '../utils/axiosInstance'
+import { signIn, signUp } from '../api/api';
 
 const Home = () => {
   const navigate = useNavigate();
   const [isSignInMode, setIsSignInMode] = useState('로그인')
+  const [btnCondition, setBtnCondition] = useState(false)
   const [inputValue, setInputValue] = useState({
     email: '',
     password: '',
     rePassword: '',
   })
+  const [emailValid, setEmailValid] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [rePasswordValid, setRePasswordValid] = useState(false);
   const {email, password, rePassword} = inputValue
+
   const handleInput = (event) => {
     const {id, value} = event.target
     setInputValue({
@@ -31,31 +36,48 @@ const Home = () => {
   const handleSubmit = async(e) => {
     e.preventDefault()
     if(isSignInMode==='로그인'){
-      try{
-        const response = await axiosInstance.post('/auth/signin',{
-          email,
-          password
-        })
-        axiosInstance.defaults.headers.common['Authorization'] = response.data.access_token
-        navigate('/todo')
-        
-      }
-      catch(error){
-        console.log(error)
-      }
+      signIn(email, password)
+      navigate('/todo')
     }else{
-      try{
-        const response = await axiosInstance.post('/auth/signup',{
-          email,
-          password
-        })
-        console.log(response)
-      }
-      catch(error){
-        console.log(error)
-      }
+      signUp(email, password)
     }
   }
+
+  // 유효성검사
+  const regEmail =
+  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const emailValidation = () => {
+    if (regEmail.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const passwordValidation = (e) => {
+    if(e.target.value.length >= 8){
+      setPasswordValid(true)
+      return 
+    }
+    setPasswordValid(false)
+    return
+  }
+  const rePasswordValidation = (e) => {
+    if(e.target.value===password){
+      return setRePasswordValid(true)
+    }
+    return setRePasswordValid(false)
+  }
+
+  useEffect(()=>{
+    if(emailValid&&passwordValid&&rePasswordValid){
+      console.log('hello')
+      setBtnCondition(true)
+    }else{
+      setBtnCondition(false)
+    }
+  },[emailValid,passwordValid,rePasswordValid])
+
   return (
     <BorderLayout>
       <FormContainer>
@@ -70,27 +92,29 @@ const Home = () => {
               <input id="password"  type='password'  onChange={handleInput} autoComplete='off' required/>
             </InputWrapper>
             <ChangeMode onClick={(e) => handleChangeMode(e)}>회원가입하러가기</ChangeMode>
+            <SubmitBtn onClick={(e) => handleSubmit(e)}>{isSignInMode}</SubmitBtn>
           </>
         ) : (
           <>
             <InputWrapper>
               <label id="email">이메일</label>
-              <input id="email"  onChange={handleInput} required/>
+              <input id="email"  onChange={handleInput} onKeyUp={emailValidation} required/>
             </InputWrapper>
 
             <InputWrapper>
               <label id="password">비밀번호</label>
-              <input id="password" type='password' onChange={handleInput}  autoComplete='off'  required/>
+              <input id="password"  type='password' onChange={handleInput} onKeyUp={passwordValidation} autoComplete='off'  required/>
             </InputWrapper>
 
             <InputWrapper>
               <label id="rePassword">비밀번호확인</label>
-              <input id="rePassword" type='password' onChange={handleInput} autoComplete='off'  required />
+              <input id="rePassword"  type='password' onChange={handleInput} onKeyUp={rePasswordValidation} autoComplete='off'  required />
             </InputWrapper>
             <ChangeMode onClick={() => handleChangeMode()}>로그인하러가기</ChangeMode>
+            <SubmitBtn onClick={(e) => handleSubmit(e)} disabled={!btnCondition}>{isSignInMode}</SubmitBtn>
           </>
         )}
-        <SubmitBtn onClick={(e) => handleSubmit(e)}>{isSignInMode}</SubmitBtn>
+        
       </FormContainer>
     </BorderLayout>
   )
@@ -125,6 +149,8 @@ const ChangeMode = styled.div`
   color: white;
   text-align: center;
 `
-const SubmitBtn = styled.button``
+const SubmitBtn = styled.button`
+
+`
 
 export default Home
