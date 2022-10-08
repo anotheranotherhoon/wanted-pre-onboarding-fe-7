8f6cfd1f7136035f51258bde6f0575069d022560
LeeChangHoon <changhoon1030@gmail.com>
2022-10-09 01:03:10 +0900

    Feat : 헤더에 토큰 추가

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-console.log(process.env.REACT_APP_URL)
+const token = window.localStorage.getItem('token') || null
+
 const axiosInstance = axios.create(
     {
     baseURL : process.env.REACT_APP_URL,
     'Content-Type' : 'application/json'
 }
 )
+
+axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
 
 export default axiosInstance
