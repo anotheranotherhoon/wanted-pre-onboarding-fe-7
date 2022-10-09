5bedb50390a13f071f5a917f0cf1106dbfeee15d
LeeChangHoon <changhoon1030@gmail.com>
2022-10-10 00:30:05 +0900

    Style : 색상 전역 변수로 설정

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,12 +5,21 @@ export const GlobalStyles = createGlobalStyle`
   ${reset}
   * {
   }
+  :root{
+    --color-blue :#0050FF;
+    --color-yellow:#FEDD89;
+    --color-mauve: #dee3f9;
+    --color-orange: #FF6C02;
+    --color-black : #000000;
+    --color-white :#ffffff;
+    
+  }
   html {
     font-size: 62.5%;
     box-sizing: border-box;
     color: rgb(51, 51, 51);
   }
-  
   body {
+    font-family: 'Noto Sans KR', sans-serif;
   }
 `;
