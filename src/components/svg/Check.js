02b29280f319d0d51b1c09268a8d67b3618a2239
LeeChangHoon <changhoon1030@gmail.com>
2022-10-10 02:02:04 +0900

    Fix : stroke-width로 다시 바꿈

--- src/components/svg/Check.js
+++ src/components/svg/Check.js
@@ -5,7 +5,7 @@ const Check = () => {
       d="M20 40L10 27.7285L14.4469 22.2715L20 29.1053L35.5531 10L40 15.457L20 40Z"
       fill="#0050FF"
     />
-    <circle cx="25" cy="25" r="22.5" stroke="#0050FF" width="5" />
+    <circle cx="25" cy="25" r="22.5" stroke="#0050FF" stroke-width="5" />
   </svg>
   )
 }
